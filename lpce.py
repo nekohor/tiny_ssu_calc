867ebee10fa746454e3599c42bb40ff5577fdfb6
nekohor <nekohor@foxmail.com>
2018-01-14 11:08:15 +0800

    refactoring?

--- lpce.py
+++ lpce.py
@@ -22,3 +22,30 @@ sns.set(rc={'font.family': [u'Microsoft YaHei']})
 sns.set(rc={'font.sans-serif': [u'Microsoft YaHei', u'Arial',
                                 u'Liberation Sans', u'Bitstream Vera Sans',
                                 u'sans-serif']})
+
+
+def Cfg_Init(line):
+    if line == 2250:
+        avg_pce_tmp_vec = np.array(
+            [600, 650, 700, 750, 800, 850,
+             900, 950, 1000, 1050, 1100, 1500])
+        elas_modu_vec = np.array(
+            [138269, 128069, 117905, 107751, 97589,
+             87415, 77232, 67054, 56909, 46829, 36863, 27067])
+        strn_rlf_cof_interp_vec = np.array(
+            [0, 0, 0, 0.056, 0.091, 0.16,
+             0.303, 0.521, 0.771, 0.968, 0.984, 0.984])
+    elif line == 1580:
+        avg_pce_tmp_vec = np.array(
+            [600, 650, 700, 750, 800, 850,
+             900, 950, 1000, 1050, 1100, 1500])
+        elas_modu_vec = np.array(
+            [138269, 128069, 117905, 107751, 97589,
+             87415, 77232, 67054, 56909, 46829, 36863, 27067])
+        strn_rlf_cof_interp_vec = np.array(
+            [0, 0, 0, 0.056, 0.091, 0.16,
+             0.303, 0.521, 0.771, 0.968, 0.984, 0.984])
+
+    else:
+        raise Exception()
+    return avg_pce_tmp_vec, elas_modu_vec, strn_rlf_cof_interp_vec
--- lrgd.py
+++ lrgd.py
@@ -24,10 +24,28 @@ sns.set(rc={'font.sans-serif': [u'Microsoft YaHei', u'Arial',
                                 u'sans-serif']})
 
 
-def Istd_Ex_PU_Prf0(std_num, strn_rlf_cof, std_ex_strn, ef_ex_pu_prf):
-    """calculate interstand exit pu prf"""
-    return ef_ex_pu_prf + std_ex_strn * (1 - strn_rlf_cof(std_num))
+def Istd_Ex_PU_Prf0(strn_rlf_cof,
+                    std_ex_strn,
+                    ef_ex_pu_prf):
+    """
+    calculate interstand exit pu prf for single stand
+    IN strn_rlf_cof   or strn_rlf_cof_vec[std]
+    IN std_ex_strn
+    IN ef_ex_pu_prf
+    """
+    return ef_ex_pu_prf + std_ex_strn * (1 - strn_rlf_cof)
 
 
-def Std_Ex_Strn1(std_num, ef_en_pu_prf, ufd_pu_prf):
-    return pce_infl_cof(std_num) * (ufd_pu_prf - ef_en_pu_prf) / prf_chg_attn_fac(std_num)
+def Std_Ex_Strn1(pce_infl_cof,
+                 prf_chg_attn_fac,
+                 ef_en_pu_prf,
+                 ufd_pu_prf):
+    """
+    calculate std_ex_strn by ef_en_pu_prf and ufd_pu_prf
+    for single stand
+    IN pce_infl_cof or pce_infl_cof_vec[std]
+    IN prf_chg_attn_fac or prf_chg_attn_fac_vec[std]
+    IN ef_en_pu_prf
+    IN ufd_pu_prf
+    """
+    return pce_infl_cof * (ufd_pu_prf - ef_en_pu_prf) / prf_chg_attn_fac
